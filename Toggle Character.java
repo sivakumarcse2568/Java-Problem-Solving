0e5011e20b57522bc550430abe4f367d7ab0153c
sivakumarcse2568 <2k22cse142@kiot.ac.in>
2025-09-13 12:24:32 +0530

    Create Toggle Character.java
    This program toggles the upperCase character into Lower and Lower to upper. TaMiL to tAmIl

---
+++ Toggle Character.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Main{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for(int i=0;i<str.length();i++){
+            if(Character.isLowerCase(str.charAt(i))){
+                System.out.print(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                System.out.print(Character.toLowerCase(str.charAt(i)));
+            }
+        }
+    }
+}
